4791bbcf594987d330611dc88e52bad792a5f333
Ramadianto <diantoorama@gmail.com>
2024-11-20 21:49:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import { testConnection } from "./Database/db.js";
+const router = require("./router")
 
 dotenv.config();
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(bodyParser.json()); 
-
+app.use(router);
 app.use(cors());
 
 // Middleware Express lainnya
