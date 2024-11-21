a00fdd1afc32e3c9907e7244c4756339c047bd36
Ramadianto <diantoorama@gmail.com>
2024-11-21 11:39:12 +0700

    appjs

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import { testConnection } from './Database/db.js'; // Import the DB connection
+import adminRoutes from './router/adminRouter.js'; // Import admin routes
+import catatanRoutes from './router/catatanRouter.js'; 
+
+dotenv.config();
+const app = express();
+
+// Middleware for parsing request body
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Admin routes
+app.use('/adminRouter', adminRoutes);
+// catatan routes
+app.use('/catatanRouter', catatanRoutes);
+
+// Start the server
+app.listen(process.env.APP_PORT, async () => {
+  await testConnection();  // Ensure database connection works
+  console.log(`Running at http://localhost:${process.env.APP_PORT}`);
+});
