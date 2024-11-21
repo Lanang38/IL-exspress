45f92dbd621b97306176fccfc051bd2a53094615
Ramadianto <diantoorama@gmail.com>
2024-11-21 18:07:30 +0700

    Create notificationRouter.js

---
+++ router/notificationRouter.js
@@ -0,0 +1,27 @@
+import express from "express";
+import {
+  tambahNotifikasi,
+  ambilSemuaNotifikasi,
+  ambilNotifikasiById,
+  editNotifikasi,
+  hapusNotifikasi,
+} from "../controllers/notification.js";
+
+const notificationRoutes = express.Router();
+
+// Tambah notifikasi
+notificationRoutes.post("/", tambahNotifikasi);
+
+// Ambil semua notifikasi
+notificationRoutes.get("/", ambilSemuaNotifikasi);
+
+// Ambil notifikasi berdasarkan ID
+notificationRoutes.get("/:id", ambilNotifikasiById);
+
+// Edit notifikasi berdasarkan ID
+notificationRoutes.put("/:id", editNotifikasi);
+
+// Hapus notifikasi berdasarkan ID
+notificationRoutes.delete("/:id", hapusNotifikasi);
+
+export default notificationRoutes;
