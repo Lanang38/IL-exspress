3c5a6e6fdd6f296adcf681722946e7e97bca0db8
Lanang38 <cokde38@gmail.com>
2024-11-22 20:54:12 +0700

    pengguna update

--- router/penggunaRouter.js
+++ router/penggunaRouter.js
@@ -1,12 +1,23 @@
 import express from "express";
-import { getUsers, deleteUser } from "../controllers/pengguna.js";
+import { 
+  getUsers, 
+  deleteUser, 
+  getUserByEmail, 
+  getUserByName 
+} from "../controllers/pengguna.js"; // Pastikan fungsi diimpor dengan benar
 
 const penggunaRoutes = express.Router();
 
-// Route to get users with pagination
+// Route untuk mendapatkan semua pengguna dengan pagination (jika ada)
 penggunaRoutes.get("/", getUsers);
 
-// Route to delete a user by ID
+// Route untuk mendapatkan pengguna berdasarkan email
+penggunaRoutes.get("/email/:email_user", getUserByEmail);
+
+// Route untuk mendapatkan pengguna berdasarkan nama
+penggunaRoutes.get("/name/:nama_user", getUserByName);
+
+// Route untuk menghapus pengguna berdasarkan email
 penggunaRoutes.delete("/:email_user", deleteUser);
 
 export default penggunaRoutes;
