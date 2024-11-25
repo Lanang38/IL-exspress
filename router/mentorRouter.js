6b5b030d8d8f3ec67f8b417e3c9fd9f366b7981d
Lanang38 <cokde38@gmail.com>
2024-11-26 02:27:01 +0700

    API Mentor

--- router/mentorRouter.js
+++ router/mentorRouter.js
@@ -5,13 +5,20 @@ import {
   hapusMentor,
   ambilSemuaMentor,
 } from "../controllers/mentor.js";
+import { mentorImages } from "../middlewares/multerMentor.js";
 
 const mentorRoutes = express.Router();
 
-// Routes untuk mentor
-mentorRoutes.post("/", tambahMentor);
-mentorRoutes.put("/:email_mentor", editMentorByEmail);
+// Route untuk menambahkan mentor dengan upload gambar
+mentorRoutes.post("/", mentorImages.single("foto_mentor"), tambahMentor);
+
+// Route untuk mengedit data mentor berdasarkan email (dengan gambar opsional)
+mentorRoutes.put("/:email_mentor", mentorImages.single("foto_mentor"), editMentorByEmail);
+
+// Route untuk menghapus mentor berdasarkan email
 mentorRoutes.delete("/:email_mentor", hapusMentor);
+
+// Route untuk mendapatkan seluruh data mentor
 mentorRoutes.get("/", ambilSemuaMentor);
 
 export default mentorRoutes;
