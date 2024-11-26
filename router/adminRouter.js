5f88225f6498a32585fcb7d88b82f763d82f06ea
Ramadianto <diantoorama@gmail.com>
2024-11-26 16:59:16 +0700

    salah dikit

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -1,25 +1,18 @@
 import express from 'express';
 import passport from 'passport';
-import {
-  loginAdmin,
-  ambilAdminByEmail,
-  ambilSemuaAdmin,
-  tambahAdmin,
-  hapusAdmin,
-  updateAdmin,
-} from '../controllers/admin.js';
-import { adminImages } from "../middlewares/multerAdmin.js";
+import { registerAdmin, ambilAdminByEmail, ambilSemuaAdmin, updateAdmin, hapusAdmin } from '../controllers/admin.js';
+import { adminImages } from '../middlewares/multerAdmin.js';
 
 const adminRoutes = express.Router();
 
 // Route Login
-adminRoutes.post('/login', loginAdmin);
+adminRoutes.post('/login', registerAdmin);
 
 // CRUD Admin
-adminRoutes.post('/', passport.authenticate('internal-rule', { session: false }), adminImages.single('foto_pr'), tambahAdmin); // Tambah admin
+adminRoutes.post('/', passport.authenticate('internal-rule', { session: false }), adminImages.single('foto_pr'), registerAdmin); // Gabungkan register dan tambah admin
 adminRoutes.get('/', passport.authenticate('internal-rule', { session: false }), ambilSemuaAdmin); // Ambil semua admin
-adminRoutes.get('/email/:email', passport.authenticate('internal-rule', { session: false }), ambilAdminByEmail); // Ambil admin by email
-adminRoutes.put('/email/:email', passport.authenticate('internal-rule', { session: false }), adminImages.single('foto_pr'), updateAdmin); // Update data admin
-adminRoutes.delete('/email/:email', passport.authenticate('internal-rule', { session: false }), hapusAdmin); // Hapus admin
+adminRoutes.get('/:email', passport.authenticate('internal-rule', { session: false }), ambilAdminByEmail); // Ambil admin by email
+adminRoutes.put('/:email', passport.authenticate('internal-rule', { session: false }), adminImages.single('foto_pr'), updateAdmin); // Update data admin
+adminRoutes.delete('/:email', passport.authenticate('internal-rule', { session: false }), hapusAdmin); // Hapus admin
 
 export default adminRoutes;
