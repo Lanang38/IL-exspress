cc926199a3fa7a99eb91064d4a5295c0931dc1a4
Lanang38 <cokde38@gmail.com>
2024-12-04 00:45:20 +0700

    matikan passport di get

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -18,7 +18,7 @@ adminRoutes.post('/daftar', adminImages.single('foto_pr'), registerAdmin); // Re
 adminRoutes.post('/login', loginAdmin); // Login admin
 
 // **Route CRUD Admin (dengan autentikasi JWT)**
-adminRoutes.get('/', passport.authenticate('internal-rule', { session: false }), ambilSemuaAdmin); // Ambil semua admin
+adminRoutes.get('/', ambilSemuaAdmin); // Ambil semua admin
 adminRoutes.get('/:email', passport.authenticate('internal-rule', { session: false }), ambilAdminByEmail); // Ambil admin berdasarkan email
 adminRoutes.put('/:email', passport.authenticate('internal-rule', { session: false }), adminImages.single('foto_pr'), updateAdmin); // Update data admin
 adminRoutes.put('/:email/password', passport.authenticate('internal-rule', { session: false }), updatePassword); // Update password admin
